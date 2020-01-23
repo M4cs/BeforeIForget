fa698d15961dc91ea98d9469e8846797f229423f
M4cs <mabridgland@protonmail.com>
2020-01-22 22:53:04 -0500

    "Auto-Commit from BIF @ 2020-01-22 22:53:04.413796 UTC"

--- beforeiforget/__main__.py
+++ beforeiforget/__main__.py
@@ -9,15 +9,17 @@ class BIF(object):
         self.start = datetime.datetime.now()
         reg = r'(\d+)'
         length_split = re.split(reg, length)
-        print(length_split)
         time_int = length_split[1]
         time_type = length_split[2]
         if time_type == "h":
             self.until = datetime.datetime.now() + datetime.timedelta(hours=int(time_int))
         elif time_type == "m":
             self.until = datetime.datetime.now() + datetime.timedelta(minutes=int(time_int))
+        elif time_type == "s":
+            self.until = datetime.datetime.now() + datetime.timedelta(seconds=int(time_int))
+        print(datetime.datetime.now())
+        print(self.until)
         interval_split = re.split(reg, interval)
-        print(interval_split)
         interval_time = interval_split[1]
         interval_type = interval_split[2]
         if interval_type == "m":
@@ -26,15 +28,15 @@ class BIF(object):
             self.interval = datetime.timedelta(minutes=int(interval_time)).total_seconds()
         else:
             self.interval = int(interval_time)
-        self.g = Git(self.cwd + '/.git')
+        print(self.interval)
+        self.g = Git(self.cwd)
         self.length = length
         self.should_run = True
         self.should_skip = False
         self.should_notify = should_notify
         t = Thread(target=self.run)
-        t.setDaemon(True)
+        t.daemon = True
         t.start()
-        print('Running BIF Here')
 
     def run(self):
         if datetime.datetime.now() >= self.until:
@@ -42,11 +44,11 @@ class BIF(object):
         while self.should_run:
             time.sleep(self.interval)
             try:
-                self.g.execute(['git', 'commit', '-am', '"Auto-Commit from BIF @ {} UTC"'.format(datetime.datetime.now())])
+                self.g.execute(command=['git', 'commit', '-am', '"Auto-Commit from BIF @ {} UTC"'.format(datetime.datetime.now())])
                 if self.should_notify:
-                    ptoaster.notify('BIF Notifier', 'BIF just commit to the local repo at {}'.format(CURRENT_DIRECTORY), display_duration_in_ms=3000, fade_in_duration=500, icon="icon_success")
+                    ptoaster.notify('BIF Notifier', 'BIF just commit to the local repo at {}'.format(CURRENT_DIRECTORY), fade_in_duration=500, icon=None)
             except:
-                ptoaster.notify('BIF Notifier', 'Commit Failed at local repo {}'.format(CURRENT_DIRECTORY), display_duration_in_ms=3000, fade_in_duration=500, icon="icon_error")
+                ptoaster.notify('BIF Notifier', 'Commit Failed at local repo {}'.format(CURRENT_DIRECTORY), fade_in_duration=500, icon=None)
 
 parser = argparse.ArgumentParser(usage="bif 5m 2h")
 parser.add_argument('interval', help='Interval to run "commit -am" on the local Repo')
